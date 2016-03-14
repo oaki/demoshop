201e08674c7a595b5d6e747e31802aa06b05cbc1
pavolbincik <pavolbincik@gmail.com>
2016-03-14 14:51:24 +0100

    delete unzipAllFiles

--- unzipAllFiles.php
+++
@@ -1,9 +0,0 @@
-<?
-$zip = new ZipArchive;
-if ($zip->open('chcemeshop_sk.zip') === TRUE) {
-    $zip->extractTo(dirname(__FILE__));
-    $zip->close();
-    echo 'ok';
-} else {
-    echo 'failed';
-}
